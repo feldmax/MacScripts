37c6d6f2027c05663279cb6a289335cc1e301823
Max Feldman <max.feldman@sap.com>
2022-08-29 15:46:54 +0300

    draft

--- package_neo.sh
+++ package_neo.sh
@@ -23,7 +23,6 @@ if ! [[ $(java -version 2>&1) =~ "build 1.8.0_" ]] && [[ $($JAVA_HOME/bin/java -
 then
     echo "Start installing | updating OpenJDK 8:"
     brew tap adoptopenjdk/openjdk
-#    brew install --cask adoptopenjdk8
     brew install --cask homebrew/cask-versions/adoptopenjdk8
     echo "$ROW_TILDA"
     echo "export JAVA_HOME=$(/usr/libexec/java_home)" >> ~/.profile
@@ -41,6 +40,7 @@ then
     command -v java
     echo "$ROW_TILDA"
 else
+    echo "Java is already installed:"
     source ~/.profile
     echo "JAVA_HOME is:"
     echo "$JAVA_HOME"
@@ -61,26 +61,27 @@ fi
 # add flags if not exist
 
 # Install NodeJS 14
-#if ! command -v node &> /dev/null
-#then
-#    echo "Start installing NodeJS 14:"
-#    brew install node@14
-#    echo 'export PATH="/opt/homebrew/opt/node@14/bin:$PATH"' >> ~/.profile
-#    echo 'export LDFLAGS="-L/opt/homebrew/opt/node@14/lib"' >> ~/.profile
-#    echo 'export CPPFLAGS="-I/opt/homebrew/opt/node@14/include"' >> ~/.profile
-#    echo 'export PATH="/opt/homebrew/opt/node@14/bin:$PATH"' >> ~/.zprofile
-#    echo 'export LDFLAGS="-L/opt/homebrew/opt/node@14/lib"' >> ~/.zprofile
-#    echo 'export CPPFLAGS="-I/opt/homebrew/opt/node@14/include"' >> ~/.zprofile
-#    source ~/.profile
-#    node --version
-#    which node
-#    echo "$ROW_TILDA"
-#else
-#    echo "NodeJS is already installed:"
-#    node --version
-#    which node
-#    echo "$ROW_TILDA"
-#fi
+if ! [[ $(node --version) =~ "v14." ]] || ! command -v node &> /dev/null
+then
+    echo "Start installing NodeJS 14:"
+    brew install node@14
+    echo 'export PATH="/opt/homebrew/opt/node@14/bin:$PATH"' >> ~/.profile
+    echo 'export PATH="/opt/homebrew/opt/node@14/bin:$PATH"' >> ~/.zprofile
+    echo 'export LDFLAGS="-L/opt/homebrew/opt/node@14/lib"' >> ~/.profile
+    echo 'export LDFLAGS="-L/opt/homebrew/opt/node@14/lib"' >> ~/.zprofile
+    echo 'export CPPFLAGS="-I/opt/homebrew/opt/node@14/include"' >> ~/.profile
+    echo 'export CPPFLAGS="-I/opt/homebrew/opt/node@14/include"' >> ~/.zprofile
+    source ~/.profile
+    node --version
+    which node
+    echo "$ROW_TILDA"
+else
+    echo "NodeJS is already installed:"
+    node --version
+    which node
+    echo "$ROW_TILDA"
+fi
+
 
 
 # Record time
