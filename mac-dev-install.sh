87d4de1c0b77255b3ea5f4e99db9db4a582dad4a
Max Feldman <max.feldman@sap.com>
2022-08-29 20:21:00 +0300

    draft

--- mac-dev-install.sh
+++ mac-dev-install.sh
@@ -99,7 +99,7 @@ fi
 
 # Create the list of packages to be installed
 packages=("package_java" "package_js" "package_python" "package_ide" "package_browsers" "package_messengers" \
-"package_miscellaneous" "package_qa" "package_neo" "package_upgrade")
+"package_miscellaneous" "package_qa" "package_devops" "package_neo" "package_upgrade")
 
 install_packs=("package_init")  #always install/update initial package
 
