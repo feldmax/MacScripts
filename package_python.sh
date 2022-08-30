7a88c7bfd2d8b7379c5043bc4882157c502e8253
Max Feldman <max.feldman@sap.com>
2022-08-30 16:41:43 +0300

    draft

--- package_python.sh
+++ package_python.sh
@@ -16,10 +16,10 @@ time1=$(date +%s)  # start time
 # Install Python and adding Python aliases if they don't exist
 echo -e "Try to install or upgrade Python:\n"
 brew install python
-grep -q  'alias python=' ~/.profile  || echo alias python='/opt/homebrew/bin/python3' >> ~/.profile
-grep -q  'alias python=' ~/.zprofile || echo alias python='/opt/homebrew/bin/python3' >> ~/.zprofile
-grep -q  'alias pip=' ~/.profile  || echo alias pip='/opt/homebrew/bin/pip3' >> ~/.profile
-grep -q  'alias pip=' ~/.zprofile || echo alias pip='/opt/homebrew/bin/pip3' >> ~/.zprofile
+#grep -q  'alias python=' ~/.profile  || echo alias python='/opt/homebrew/bin/python3' >> ~/.profile
+#grep -q  'alias python=' ~/.zprofile || echo alias python='/opt/homebrew/bin/python3' >> ~/.zprofile
+#grep -q  'alias pip=' ~/.profile  || echo alias pip='/opt/homebrew/bin/pip3' >> ~/.profile
+#grep -q  'alias pip=' ~/.zprofile || echo alias pip='/opt/homebrew/bin/pip3' >> ~/.zprofile
 source ~/.profile
 python3 --version
 which python3
@@ -30,8 +30,8 @@ if ! command -v jupyter-lab &> /dev/null
 then
     echo "Start installing Jupyter-Lab:"
     brew install jupyterlab
-    grep -q  'jplab=' ~/.profile  || echo alias jplab='/opt/homebrew/bin/jupyter-lab' >> ~/.profile
-    grep -q  'jplab=' ~/.zprofile || echo alias jplab='/opt/homebrew/bin/jupyter-lab' >>  ~/.zprofile
+#    grep -q  'jplab=' ~/.profile  || echo alias jplab='/opt/homebrew/bin/jupyter-lab' >> ~/.profile
+#    grep -q  'jplab=' ~/.zprofile || echo alias jplab='/opt/homebrew/bin/jupyter-lab' >>  ~/.zprofile
     source ~/.profile
     jupyter --version
     echo "$ROW_TILDA"
