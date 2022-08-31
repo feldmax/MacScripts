85443ef4f6c2d97b12332948f91e418d9d1269db
Max Feldman <max.feldman@sap.com>
2022-09-01 00:06:44 +0300

    v. 1.0 Beta

--- package_custom.sh
+++ package_custom.sh
@@ -20,7 +20,7 @@ you need. The installer will process this package directly from your computer, s
 you don't need to upload it to GitHub or anywhere else. You can also use this
 package to overwrite any program installed with shared packages or change the
 default configuration if you need to.
-Follow the instructions on this web page: <TBD>
+Follow the instructions on this web page: <\e]8;;https://github.com/feldmax/MacScripts#how-to-create-and-run-custom-package\aHow to create and run custom package\e]8;;\a>
 and just change the commented code examples below. 👈
 
 "
